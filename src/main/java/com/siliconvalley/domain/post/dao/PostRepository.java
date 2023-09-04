935c56bc156d3205be1a1b182a336dbfb192345b
이명우 <dlauddndlek@naver.com>
2023-09-04 18:03:25 +0900

    feat:게시물 CRUD 초기 세팅 [BACK-146] Related to : #146

---
+++ src/main/java/com/siliconvalley/domain/post/dao/PostRepository.java
@@ -0,0 +1,7 @@
+package com.siliconvalley.domain.post.dao;
+
+import com.siliconvalley.domain.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
