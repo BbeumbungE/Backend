acb2a91664329ee22d033ee657f0699dd602c840
이명우 <dlauddndlek@naver.com>
2023-09-05 15:02:56 +0900

    fix: 컨트롤러 api 엔드포인트 수정 #BACK-146

--- src/main/java/com/siliconvalley/domain/post/controller/PostController.java
+++ src/main/java/com/siliconvalley/domain/post/controller/PostController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
 }
