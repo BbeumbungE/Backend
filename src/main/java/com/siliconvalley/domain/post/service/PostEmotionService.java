bf7383a59ba0f9e5bb19d982e589381c702f421c
이명우 <dlauddndlek@naver.com>
2023-09-06 11:06:24 +0900

    fix: 따옴표 및 미사용 import 제거

--- src/main/java/com/siliconvalley/domain/post/service/PostEmotionService.java
+++ src/main/java/com/siliconvalley/domain/post/service/PostEmotionService.java
@@ -6,10 +6,8 @@ import com.siliconvalley.domain.post.dao.EmotionRepository;
 import com.siliconvalley.domain.post.dao.EmotionTypeFindDao;
 import com.siliconvalley.domain.post.dao.PostFindDao;
 import com.siliconvalley.domain.post.domain.Emotion;
-import com.siliconvalley.domain.post.domain.EmotionType;
 import com.siliconvalley.domain.post.domain.Post;
 import com.siliconvalley.domain.post.dto.EmotionCreatedResponse;
-import com.siliconvalley.domain.post.dto.PostResponse;
 import com.siliconvalley.domain.post.exception.IllegalDeleteException;
 import com.siliconvalley.domain.profile.domain.Profile;
 import com.siliconvalley.global.common.dto.Response;
@@ -36,7 +34,7 @@ public class PostEmotionService {
     public Response cancelEmote(Long emotionId, Long requestProfileId){
         Emotion emotion = emotionFindDao.findById(emotionId);
         if (emotion.getProfile().getId() != requestProfileId){
-            throw new IllegalDeleteException("요청 ID와 감정표현 프로필 ID가 맞지않음")
+            throw new IllegalDeleteException("요청 ID와 감정표현 프로필 ID가 맞지않음");
         }
         emotionRepository.delete(emotion);
         return Response.of(EmotionCode.CANCEL_EMOTION_SUCCESS, null);
