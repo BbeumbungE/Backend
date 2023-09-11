e4b68efa625f0e7644b98e27773aa31f1219447b
이명우 <dlauddndlek@naver.com>
2023-09-11 14:03:00 +0900

    feat: S3 연동 [BACK-189] Resolves: #189

--- src/main/java/com/siliconvalley/SiliconValleyApplication.java
+++ src/main/java/com/siliconvalley/SiliconValleyApplication.java
@@ -7,6 +7,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class SiliconValleyApplication {
+
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SiliconValleyApplication.class, args);
 	}
---
+++ src/main/java/com/siliconvalley/domain/image/service/S3ImageUploadService.java
@@ -0,0 +1,34 @@
+package com.siliconvalley.domain.image.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3ImageUploadService {
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadFile(MultipartFile multipartFile) throws IOException{
+        String fileName = multipartFile.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        amazonS3Client.putObject(bucket, fileName, multipartFile.getInputStream(), metadata);
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    public void deleteImage(String originalFileName){
+        amazonS3Client.deleteObject(bucket, originalFileName);
+    }
+}
