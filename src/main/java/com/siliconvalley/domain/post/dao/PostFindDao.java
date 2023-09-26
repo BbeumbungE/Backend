225d1ff82d7098429db84d0fcf67c68441c242c3
이명우 <dlauddndlek@naver.com>
2023-09-26 17:05:27 +0900

    fix: 주제별 게시물 조회 누락 정보 수정 [BACK-257] Resolves : issue #257

--- src/main/java/com/siliconvalley/domain/post/dao/PostFindDao.java
+++ src/main/java/com/siliconvalley/domain/post/dao/PostFindDao.java
@@ -1,11 +1,10 @@
 package com.siliconvalley.domain.post.dao;
 
 import com.siliconvalley.domain.item.subject.dao.SubjectFindDao;
-import com.siliconvalley.domain.item.subject.domain.Subject;
+import com.siliconvalley.domain.item.subject.dto.SubjectCachingDto;
 import com.siliconvalley.domain.post.code.PostCode;
 import com.siliconvalley.domain.post.domain.Post;
 import com.siliconvalley.domain.post.dto.PostListResponse;
-import com.siliconvalley.domain.post.dto.PostDetailResponse;
 import com.siliconvalley.domain.post.dto.PostSubjectResponse;
 import com.siliconvalley.domain.post.exception.PostNotFoundException;
 import com.siliconvalley.global.common.dto.Response;
@@ -15,9 +14,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +24,7 @@ public class PostFindDao {
 
     private final PostRepository postRepository;
     private final PostCustomRepository postCustomRepository;
+    private final SubjectFindDao subjectFindDao;
 
     public Post findById(Long targetId){
         final Optional<Post> post = postRepository.findById(targetId);
@@ -40,7 +39,8 @@ public class PostFindDao {
 
     public Response getPostsBySubjectName(Long subjectId, Pageable pageable) {
         Page<PostListResponse> postListResponses = postCustomRepository.findBySubjectId(subjectId, pageable);
-        return Response.of(PostCode.POST_RETRIEVE_SUCCESS, postListResponses);
+        SubjectCachingDto dto = subjectFindDao.findSubjectForCachingById(subjectId);
+        return Response.of(PostCode.POST_RETRIEVE_SUCCESS, new PostSubjectResponse(dto, postListResponses));
     }
 
 }
