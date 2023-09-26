a5398a990562d86b53b14aa7139b8d83c0e89010
이명우 <dlauddndlek@naver.com>
2023-09-26 17:32:45 +0900

    임시

---
+++ src/test/java/com/siliconvalley/profile/ProfileNameServiceTests.java
@@ -0,0 +1,12 @@
+package com.siliconvalley.profile;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+public class ProfileNameServiceTests {
+
+}
