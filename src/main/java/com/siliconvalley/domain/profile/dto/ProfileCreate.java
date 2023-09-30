67dacc262916b96bb3905cd57e75d2204da025e7
ah9mon <eomgyeol97@gmail.com>
2023-10-01 01:05:13 +0900

    Feat: Profile 관리 테스트 추가

--- src/main/java/com/siliconvalley/domain/profile/dto/ProfileCreate.java
+++ src/main/java/com/siliconvalley/domain/profile/dto/ProfileCreate.java
@@ -17,7 +17,7 @@ public class ProfileCreate {
     @Valid
     private String profileName;
 
-    ProfileCreate(@Valid String profileName, @Valid Long myItemId) {
+    ProfileCreate(@Valid String profileName) {
         this.profileName = profileName;
     }
 
